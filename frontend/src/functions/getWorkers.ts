c8c583d94953777837c396f6c3c28c30f31cafa4
Lauther <106201226+Lauther99@users.noreply.github.com>
2025-06-18 11:57:45 -0500

    Update getWorkers.ts

--- frontend/src/functions/getWorkers.ts
+++ frontend/src/functions/getWorkers.ts
@@ -2,9 +2,9 @@ export const workers: { [key: string]: string }[] = [
   { index: "001", nombre: "SOL SARANGO", dni: "75865823" },
   { index: "002", nombre: "DIANA IPANAQUE", dni: "03892121" },
   { index: "003", nombre: "RONALD ROMERO", dni: "47809277" },
-  { index: "004", nombre: "ALEJANDRO HIDALGO LIZAMA", dni: "73520262" },
-  { index: "005", nombre: "STACY LIZAMA", dni: "60843575" },
-  { index: "006", nombre: "JOSE VALLADARES", dni: "25729326" },
-  { index: "007", nombre: "BRENDY CHERO", dni: "43857335" },
-  { index: "008", nombre: "YOVANY RIVAS", dni: "03898576" }
+  // { index: "004", nombre: "ALEJANDRO HIDALGO LIZAMA", dni: "73520262" },
+  { index: "004", nombre: "STACY LIZAMA", dni: "60843575" },
+  { index: "005", nombre: "JOSE VALLADARES", dni: "25729326" },
+  { index: "006", nombre: "BRENDY CHERO", dni: "43857335" },
+  { index: "007", nombre: "YOVANY RIVAS", dni: "03898576" }
 ];
