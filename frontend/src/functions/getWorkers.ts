6b3d4fc9ba26cf7ed4e71a132670fbbb24d33b46
Lauther <106201226+Lauther99@users.noreply.github.com>
2025-12-02 13:56:49 -0500

    Update worker entries in getWorkers.ts

--- frontend/src/functions/getWorkers.ts
+++ frontend/src/functions/getWorkers.ts
@@ -6,15 +6,13 @@ export interface Workers {
 }
 
 export const workers: Workers[] = [
-  { index: "001", nombre: "SOL SARANGO", dni: "75865823", role: "worker" },
+  { index: "001", nombre: "LUCIA BORRERO", dni: "46185432", role: "worker" },
   { index: "002", nombre: "DIANA IPANAQUE", dni: "03892121", role: "worker" },
   { index: "003", nombre: "RONALD ROMERO", dni: "47809277", role: "worker" },
-  { index: "004", nombre: "KAIRO AZCÁRATE ESPINOZA", dni: "75584723", role: "worker" },
-  { index: "005", nombre: "STACY LIZAMA", dni: "60843575", role: "worker" },
-  { index: "006", nombre: "JOSE VALLADARES", dni: "25729326", role: "worker" },
-  { index: "007", nombre: "BRENDY CHERO", dni: "43857335", role: "worker" },
-  { index: "008", nombre: "YOVANY RIVAS", dni: "03898576", role: "worker" },
-  { index: "009", nombre: "JEANCARLO QUEREVALU", dni: "71538331", role: "worker" },
+  { index: "004", nombre: "JOSE VALLADARES", dni: "25729326", role: "worker" },
+  { index: "005", nombre: "BRENDY CHERO", dni: "43857335", role: "worker" },
+  { index: "006", nombre: "YOVANY RIVAS", dni: "03898576", role: "worker" },
+  { index: "007", nombre: "JEANCARLO QUEREVALU", dni: "71538331", role: "worker" },
   { index: "admin1", nombre: "JOSE BENITES", dni: "88888888", role: "admin" },
   { index: "admin2", nombre: "LAUTHER VALLADARES", dni: "99999999", role: "admin" },
 ];
